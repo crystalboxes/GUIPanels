c1c961c13360a222691fdd3cbe138d63c976578a
crystalboxes <ursus95@gmail.com>
2020-03-08 20:36:36 +0200

    Line rendering fixed

--- Unity/ShapeDrawing.cs
+++ Unity/ShapeDrawing.cs
@@ -41,56 +41,42 @@ namespace GUIPanels.Unity
       perpendicular.Normalize();
 
       float halfWidth = width * 0.5f;
-      var start0 = p0 + dir * halfWidth;
-      var start1 = p0 - dir * halfWidth;
+      var start0 = p0 + perpendicular * halfWidth;
+      var start1 = p0 - perpendicular * halfWidth;
 
-      var end0 = p1 + dir * halfWidth;
-      var end1 = p1 - dir * halfWidth;
+      var end0 = p1 + perpendicular * halfWidth;
+      var end1 = p1 - perpendicular * halfWidth;
 
       // draw circles
       var res = CircleResolution;
       CircleResolution = 8;
-      // DrawCircle(p0, halfWidth, color);
-      // DrawCircle(p1, halfWidth, color);
       CircleResolution = res;
 
       RenderMaterial.SetPass(0);
-      GL.Begin(GL.LINES);
+      GL.Begin(GL.TRIANGLES);
       GL.Color(color);
+
+      DrawCircleCommands(p0, halfWidth, color);
+      DrawCircleCommands(p1, halfWidth, color);
+
 
       GL.Vertex3(start0.x, start0.y, 0);
       GL.Vertex3(end0.x, end0.y, 0);
-      GL.End();
-      
-      GL.Begin(GL.LINES);
-
-      GL.Vertex3(start1.x, start1.y, 0);
       GL.Vertex3(end1.x, end1.y, 0);
-      // GL.Vertex3(end1.x, end1.y, 0);
 
-      // GL.Vertex3(start0.x, start0.y, 0);
-      // GL.Vertex3(end1.x, end1.y, 0);
-      // GL.Vertex3(start1.x, start1.y, 0);
+      GL.Vertex3(start0.x, start0.y, 0);
+      GL.Vertex3(end1.x, end1.y, 0);
+      GL.Vertex3(start1.x, start1.y, 0);
 
       GL.End();
-
-      UnityEngine.Debug.LogFormat("{0} {1}, {2} {3}, {4} {5}, {6} {7}",
-      start0.x, start0.y, start1.x, start1.y, end0.x, end0.y, end1.x, end1.y
-      );
-
     }
     public override void DrawArc(Vec2 center, float radius, float startAngle, float endAngle, float width, Col color)
     {
 
     }
-    public override void DrawCircle(Vec2 center, float radius, Col color)
-    {
-      center = Aspect.Adjust(center);
-      radius = Aspect.Adjust(radius);
 
-      RenderMaterial.SetPass(0);
-      GL.Begin(GL.TRIANGLES);
-      GL.Color(color);
+    void DrawCircleCommands(Vec2 center, float radius, Col color)
+    {
       float angle = 0f;
       const float twopi = 2f * (float)System.Math.PI;
       float incr = twopi / (float)CircleResolution;
@@ -105,6 +91,16 @@ namespace GUIPanels.Unity
           center.y + (float)System.Math.Cos(angle + incr) * radius, 0);
         angle += incr;
       }
+    }
+    public override void DrawCircle(Vec2 center, float radius, Col color)
+    {
+      center = Aspect.Adjust(center);
+      radius = Aspect.Adjust(radius);
+
+      RenderMaterial.SetPass(0);
+      GL.Begin(GL.TRIANGLES);
+      GL.Color(color);
+      DrawCircleCommands(center, radius, color);
       GL.End();
     }
     public override void DrawRing(Vec2 center, float radius, float innerRadius, Col color)
